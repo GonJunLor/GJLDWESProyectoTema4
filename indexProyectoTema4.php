e9097d3fd4a506dc63eabfd262ee8715fac65012
GonJunLor <gonzalo.junlor@educa.jcyl.es>
2025-10-30 13:04:22 +0100

    Inicio

---
+++ indexProyectoTema4.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<!--
+    Autor: Gonzalo Junquera Lorenzo
+    Fecha modificación: 09/04/2025
+    Descripción: Indice general de la asignatura
+-->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Indice general de la asignatura">
+    <meta name="author" content="Gonzalo Junquera Lorenzo">
+    <meta name="keywords" content="indice, daw1, edd">
+    <meta name="generator" content="visual studio code">
+    <link rel="icon" type="image/png" href="webroot/media/favicon/favicon-32x32.png">
+    <link rel="stylesheet" href="webroot/css/fonts.css">
+    <link rel="stylesheet" href="webroot/css/estilosHome.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" type="text/css" rel="stylesheet">
+    <link rel="alternate" href="rss.xml" type="application/rss+xml" title="RSS">
+    <title>Gonzalo Junquera Lorenzo</title>
+</head>
+<body>
+    <div id="aviso">CURSO 2025/2026 -- DAW 2 -- I.E.S. LOS SAUCES</div>
+    <nav>
+        <div><a href="/">Menu</a></div>
+        <h2>DWES - Tema 4</h2>
+        <h2>Gonzalo Junquera Lorenzo</h2>
+    </nav>
+    <main>
+        <table>
+            <thead>
+            <th></th>
+            <th>ED</th>
+            <th colspan="2">EE</th>
+            </thead>
+            <tr>
+                <td>S.C.C</td>
+                <td><a href="codigoPHP/ejercicio01.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio01.php"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>S.C.C</td>
+                <td><a href="codigoPHP/ejercicio01.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio01.php"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>S.C.C</td>
+                <td><a href="codigoPHP/ejercicio01.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio01.php"></td>
+                <td></td>
+            </tr>
+        </table>
+        <table>
+            <thead>
+            <th>Num</th>
+            <th>Descripción</th>
+            <th colspan="2">PDO</th>
+            <th colspan="2">MySQLi</th>
+            </thead>
+            <tr>
+                <td>1</td>
+                <td>Conexión a la base de datos.</td>
+                <td><a href="codigoPHP/ejercicio01.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio01.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Mostrar el contenido de la tabla Departamento y el número de registros.</td>
+                <td><a href="codigoPHP/ejercicio02.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio02.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Formulario para añadir un departamento a la tabla Departamento.</td>
+                <td><a href="codigoPHP/ejercicio03.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio03.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Formulario de búsqueda de departamentos por descripción .</td>
+                <td><a href="codigoPHP/ejercicio04.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio04.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Pagina web que añade tres registros a nuestra tabla Departamento.</td>
+                <td><a href="codigoPHP/ejercicio05.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio05.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Pagina web que cargue registros en la tabla Departamento.</td>
+                <td><a href="codigoPHP/ejercicio06.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio06.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>Página web que toma datos (código y descripción) de un fichero xml y los añade a la tabla.</td>
+                <td><a href="codigoPHP/ejercicio07.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio07.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Página web que toma datos (código y descripción) de la tabla Departamento.</td>
+                <td><a href="codigoPHP/ejercicio08.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio08.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+                <td><a href="codigoPHP/ejercicio00.php"><img src="webroot/media/images/play.png" alt="boton_play"></a></td>
+                <td><a href="mostrarcodigo/muestraejercicio00.php"><img src="webroot/media/images/code.webp" alt="boton_code"></a></td>
+            </tr>
+        </table>
+    </main>
+    <footer id="pieEDD">
+        <div>
+            <a href="/feed/rss.xml" type="application/rss+xml" target="_blank"><i class="fa-solid fa-rss"></i></a>
+            <a href="https://github.com/GonJunLor/108EDDProyectoEDD" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://mogutable.com/" target="_blank"><i class="fa-solid fa-house"></i></a>
+            <a id="w3c" href="https://validator.w3.org/check?uri=https://daw108.ieslossauces.es/108EDDProyectoEDD/index.html" target="_blank">          
+                <img src="/webroot/media/images/W3C.png" alt="Validado por W3C" width="55" >        
+            </a>
+        </div>
+        2024-25 IES LOS SAUCES. &#169;Todos los derechos reservados. 
+        <div>
+            <a href="/index.html">
+            <address style="display: inline;">Gonzalo Junquera Lorenzo</address>
+            </a>
+            <time datetime="2025-04-09">09-04-2025</time>
+        </div>
+    </footer>
+</body>
+</html>
