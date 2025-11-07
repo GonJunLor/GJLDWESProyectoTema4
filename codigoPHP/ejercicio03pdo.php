4dc1b6ba49d8505678e73b9b2709ab3691548552
GonJunLor <gonzalo.junlor@educa.jcyl.es>
2025-11-07 13:50:59 +0100

    fix: añadir campos

--- codigoPHP/ejercicio03pdo.php
+++ codigoPHP/ejercicio03pdo.php
@@ -22,25 +22,33 @@
         const PASSWORD = '5813Libro-Puro';
         // const PASSWORD = 'paso';
 
-        // uso una variable para que la misma línea de codigo me sirva en casa y en clase al usar server_addr
+        // uso una variable para que la misma línea de T02_CodDepartamento me sirva en casa y en clase al usar server_addr
         $DSN = 'mysql:host='.$_SERVER['SERVER_ADDR'].'; dbname=DBGJLDWESProyectoTema4';
        
         $entradaOK = true; //Variable que nos indica que todo va bien
         $aErrores = [  //Array donde recogemos los mensajes de error
-            'codigo' => '', 
-            'descripcion' => ''
+            'T02_CodDepartamento' => '', 
+            'T02_DescDepartamento' => '',
+            'T02_FechaCreacionDepartamento' => '',
+            'T02_VolumenDeNegocio' => '',
+            'T02_FechaBajaDepartamento' => ''
         ];
         $aRespuestas=[ //Array donde recogeremos la respuestas correctas (si $entradaOK)
-            'codigo' => '', 
-            'descripcion' => ''
+            'T02_CodDepartamento' => '', 
+            'T02_DescDepartamento' => '',
+            'T02_FechaCreacionDepartamento' => '',
+            'T02_VolumenDeNegocio' => '',
+            'T02_FechaBajaDepartamento' => ''
         ]; 
         
         //Para cada campo del formulario: Validar entrada y actuar en consecuencia
         if (isset($_REQUEST["enviar"])) {//Código que se ejecuta cuando se envía el formulario
 
             // Validamos los datos del formulario
-            $aErrores['codigo']= validacionFormularios::comprobarAlfabetico($_REQUEST['codigo'],3,0,1,);
-            $aErrores['descripcion']= validacionFormularios::comprobarAlfabetico($_REQUEST['descripcion'],255,0,1);
+            $aErrores['T02_CodDepartamento']= validacionFormularios::comprobarAlfabetico($_REQUEST['T02_CodDepartamento'],3,0,1,);
+            $aErrores['T02_DescDepartamento']= validacionFormularios::comprobarAlfabetico($_REQUEST['T02_DescDepartamento'],255,0,1);
+            // (si no puedo desde cliente) modificar el número para cambiar la coma por punto.
+            $aErrores['T02_VolumenDeNegocio']= validacionFormularios::comprobarFloat($_REQUEST['T02_VolumenDeNegocio']);
             
             foreach($aErrores as $campo => $valor){
                 if(!empty($valor)){ // Comprobar si el valor es válido
@@ -48,25 +56,25 @@
                 } 
             }
 
-            if (empty($aErrores['codigo'])) {
+            if (empty($aErrores['T02_CodDepartamento'])) {
                 
                 try {
                     $miDB = new PDO($DSN,USERNAME,PASSWORD);
                     
                     $consulta = $miDB->prepare("select * from T02_Departamento where T02_CodDepartamento=?");
-                    $consulta->bindParam(1,$_REQUEST['codigo']);
+                    $consulta->bindParam(1,$_REQUEST['T02_CodDepartamento']);
                     $consulta->execute();
                     
-                    // Comprobamos si existe el codigo en la BBDD
+                    // Comprobamos si existe el T02_CodDepartamento en la BBDD
                     $registro = $consulta->fetch();
-                    if($registro[0]==strtoupper($_REQUEST['codigo'])){
-                        $aErrores['codigo']='El código ya existe un la BBDD';
+                    if($registro[0]==strtoupper($_REQUEST['T02_CodDepartamento'])){
+                        $aErrores['T02_CodDepartamento']='El código ya existe un la BBDD';
                         $entradaOK = false;
                     } // si existe guardamos un error para mostrarlo en el formulario
                     
                 } catch (PDOException $miExceptionPDO) {
                     // temporalmente ponemos estos errores para que se muestren en pantalla
-                    $aErrores['codigo']= 'Error: '.$miExceptionPDO->getMessage().'con código de error: '.$miExceptionPDO->getCode();
+                    $aErrores['T02_CodDepartamento']= 'Error: '.$miExceptionPDO->getMessage().'con código de error: '.$miExceptionPDO->getCode();
                     $entradaOK = false;
                 } finally {
                     unset($miDB);
@@ -80,16 +88,18 @@
         if($entradaOK){ //Cargar la variable $aRespuestas y tratamiento de datos OK
             
             // Recuperar los valores del formulario
-            $aRespuestas['codigo'] = strtoupper($_REQUEST['codigo']);
-            $aRespuestas['descripcion'] = "Departamento de ".$_REQUEST['descripcion'];
+            $aRespuestas['T02_CodDepartamento'] = strtoupper($_REQUEST['T02_CodDepartamento']);
+            $aRespuestas['T02_DescDepartamento'] = "Departamento de ".$_REQUEST['T02_DescDepartamento'];
+            $aRespuestas['T02_CodDepartamento'] = $_REQUEST['T02_VolumenDeNegocio'];
             
             try {
                     $miDB = new PDO($DSN,USERNAME,PASSWORD);
 
                     // conexion a la BBDD e insertar un registro
-                    $consulta = $miDB->prepare("insert into T02_Departamento values (?,?,now(),0,null)");
-                    $consulta->bindParam(1,$aRespuestas['codigo']);
-                    $consulta->bindParam(2,$aRespuestas['descripcion']);
+                    $consulta = $miDB->prepare("insert into T02_Departamento values (?,?,now(),10,null)");
+                    $consulta->bindParam(1,$aRespuestas['T02_CodDepartamento']);
+                    $consulta->bindParam(2,$aRespuestas['T02_DescDepartamento']);
+                    // $consulta->bindParam(3,(empty($aRespuestas['T02_VolumenDeNegocio'])?"0":"10"));
                     
                     if($consulta->execute()){
                         echo 'Nuevo departamento creado con éxito';
@@ -100,7 +110,7 @@
                     
                 } catch (PDOException $miExceptionPDO) {
                     // temporalmente ponemos estos errores para que se muestren en pantalla
-                    $aErrores['codigo']= 'Error: '.$miExceptionPDO->getMessage().'con código de error: '.$miExceptionPDO->getCode();
+                    $aErrores['T02_CodDepartamento']= 'Error: '.$miExceptionPDO->getMessage().'con código de error: '.$miExceptionPDO->getCode();
                     $entradaOK = false;
                 } finally {
                     unset($miDB);
@@ -113,22 +123,70 @@
             //Mostrar formulario
             //Mostrar los datos tecleados correctamente en intentos anteriores
             //Mostrar mensajes de error (si los hay y el formulario no se muestra por primera vez)
+            $oFechaActual = new DateTime();
             ?>
                 <h2>Nuevo departamento</h2>
                 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post"> 
-                    <label for="codigo">Código:</label>
-                    <input type="text" id="codigo" class="obligatorio" name="codigo" value="<?php echo $_REQUEST['codigo']??'' ?>"><span class="error"><?php echo $aErrores['codigo'] ?></span>
+                    <label for="T02_CodDepartamento">Código:</label>
+                    <input type="text" id="T02_CodDepartamento" class="obligatorio" name="T02_CodDepartamento" value="<?php echo $_REQUEST['T02_CodDepartamento']??'' ?>">
+                    <span class="error"><?php echo $aErrores['T02_CodDepartamento'] ?></span>
                     <br>
-                    <label for="descripcion">Descripción: Departamento de </label>
-                    <input type="text" name="descripcion" class="obligatorio" value="<?php echo $_REQUEST['descripcion']??'' ?>"><span class="error"><?php echo $aErrores['descripcion'] ?></span>
+                    <label for="T02_DescDepartamento">Descripción: Departamento de </label>
+                    <input type="text" name="T02_DescDepartamento" class="obligatorio" value="<?php echo $_REQUEST['T02_DescDepartamento']??'' ?>">
+                    <span class="error"><?php echo $aErrores['T02_DescDepartamento'] ?></span>
                     <br>
-                    <input type="submit" value="Crear" name="enviar">
+                    <label for="T02_FechaCreacionDepartamento">Fecha de alta:</label>
+                    <input type="text" id="T02_FechaCreacionDepartamento" name="T02_FechaCreacionDepartamento" value="<?php echo $oFechaActual->format("d/m/Y") ?>" readonly>
+                    <br>
+                    <label for="T02_VolumenDeNegocio">Volumen de negocio:</label>
+                    <input type="text" id="T02_VolumenDeNegocio" name="T02_VolumenDeNegocio" value="<?php echo $_REQUEST['T02_VolumenDeNegocio']??'' ?>">
+                    <span class="error"><?php echo $aErrores['T02_VolumenDeNegocio'] ?></span>
+                    <br>
+                    <input type="submit" value="Aceptar" name="enviar">
+                    <input type="submit" value="Cancelar" name="volver">
                 </form>
 
             <?php
+            
         }
        ?>
     </main>
+    <?php 
+        try {
+            $miDB = new PDO($DSN,USERNAME,PASSWORD);
+            
+            $consulta = $miDB->prepare("select * from T02_Departamento");
+            $consulta->execute();
+
+            echo '<table>';
+            echo '<tr>';
+            echo '<th>Código</th>';
+            echo '<th>Departamento</th>';
+            echo '<th>Fecha de Creacion</th>';
+            echo '<th>Volumen de Negocio</th>';
+            echo '<th>Fecha de Baja</th>';
+            echo '</tr>';
+
+            while ($registro = $consulta->fetch()) {
+                echo '<tr>';
+                echo '<td>'.$registro['T02_CodDepartamento'].'</td>';
+                echo '<td>'.$registro["T02_DescDepartamento"].'</td>';
+                echo '<td>'.$registro["T02_FechaCreacionDepartamento"].'</td>';
+                // formateamos el float para que se vea en €
+                echo '<td>'.number_format($registro["T02_VolumenDeNegocio"],2,',','.').' €</td>';
+                echo '<td>'.$registro["T02_FechaBajaDepartamento"].'</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+
+        } catch (PDOException $miExceptionPDO) {
+            echo 'Error: '.$miExceptionPDO->getMessage();
+            echo '<br>';
+            echo 'Código de error: '.$miExceptionPDO->getCode();
+        } finally {
+            unset($miDB);
+        }
+    ?>
 </body>
 <head>
     <meta charset="UTF-8">
@@ -136,7 +194,7 @@
     <link rel="stylesheet" href="../webroot/css/estilos.css">
     <title>Gonzalo Junquera Lorenzo</title>
     <style>
-        #telefono, #nombre {
+        .obligatorio {
             background-color: lightgoldenrodyellow;
         }
         main{
@@ -192,10 +250,10 @@
             width: 20px;
             height: 18px;
         }
-        input[name="enviar"], button{
+        input[type="submit"], button{
             padding: 10px 25px;
             font-size: 1.2rem;
-            margin: 20px 120px;
+            margin: 30px 90px;
             border-radius: 20px;
             background-color: #4988bbff;
             color: white;
