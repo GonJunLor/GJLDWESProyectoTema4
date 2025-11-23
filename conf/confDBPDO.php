9c21b14b2ec5bf92e484edf81cb83cf9138c028a
Gonzalo Junquera Lorenzo <gonzalo.junlor@educa.jcyl.es>
2025-11-23 13:33:01 +0100

    test: modificar archivo ignorado

--- conf/confDBPDO.php
+++ conf/confDBPDO.php
@@ -3,4 +3,5 @@
 define('DSN', 'mysql:host=' . $_SERVER['SERVER_ADDR'] . '; dbname=DBGJLDWESProyectoTema4');
 define('USERNAME','userGJLDWESProyectoTema4');
 define('PASSWORD','5813Libro-Puro');
+
 ?>
